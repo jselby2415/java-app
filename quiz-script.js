8770162a702b460e27f6b8bdd4d0e9efd038f4ea
Jared Selby <jaredselby@pmacss-MBP.hsd1.pa.comcast.net>
2020-12-05 21:59:40 -0500

    java app

--- quiz-script.js
+++ quiz-script.js
@@ -2,18 +2,18 @@ var currentQuestion = 0;
 var score = 0;
 var totQuestions = questions.length
 
-var container = document.getElementbyId('quizContainer');
-var questionEl = document.getElementbyId('question');
-var opt1 = document.getElementbyId('opt1');
-var opt2 = document.getElementbyId('opt2');
-var opt3 = document.getElementbyId('opt3');
-var opt4 = document.getElementbyId('opt4');
-var nextButton = document.getElementbyId('nextbutton');
-var resultCont = document.getElementbyId('result');
+var container = document.getElementById('quizContainer');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButton = document.getElementById('nextbutton');
+var resultCont = document.getElementById('result');
 
 function loadQuestion (questionIndex){
 	var q = questions[questionIndex];
-	questionsEl.textContent = (questionIndex + 1) + '. ' + q.question;
+	questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
 	opt1.textContent = q.option1;
 	opt2.textContent = q.option2;
 	opt3.textContent = q.option3;
